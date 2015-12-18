7fb0d90ab6451e34cd9dea2446997569a7290417
Piotr Smialkowski <piotr.smialkowski@ts.fujitsu.com>
2015-12-18 17:19:54 +0100

    Make it working

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,28 @@
 System.register(['sapps.core.api'], function (_export) {
     'use strict';
 
-    var IHttpConnection;
+    var IHttpConnection, Connection;
+
+    var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
     _export('start', start);
 
     _export('stop', stop);
 
+    function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+    function checkStatus(response) {
+        if (response.status >= 200 && response.status < 300) {
+            return response;
+        } else {
+            var error = new Error(response.statusText);
+            error.response = response;
+            throw error;
+        }
+    }
+
     function start(ctx) {
-        ctx.services.register(IHttpConnection, {
-            get: fetch
-        });
+        ctx.services.register(IHttpConnection, new Connection());
     }
 
     function stop(ctx) {}
@@ -19,6 +31,36 @@ System.register(['sapps.core.api'], function (_export) {
         setters: [function (_sappsCoreApi) {
             IHttpConnection = _sappsCoreApi.IHttpConnection;
         }],
-        execute: function () {}
+        execute: function () {
+            Connection = (function () {
+                function Connection() {
+                    _classCallCheck(this, Connection);
+                }
+
+                _createClass(Connection, [{
+                    key: 'get',
+                    value: function get(url, params) {
+                        url = 'api/v1' + url + '.json';
+                        return fetch(url).then(checkStatus).then(function (response) {
+                            return response.json();
+                        });
+                    }
+                }, {
+                    key: 'post',
+                    value: function post(url, params) {}
+                }, {
+                    key: 'head',
+                    value: function head(url, params) {}
+                }, {
+                    key: 'delete',
+                    value: function _delete(url, params) {}
+                }, {
+                    key: 'put',
+                    value: function put(url, params) {}
+                }]);
+
+                return Connection;
+            })();
+        }
     };
 });
--- src/index.js
+++ src/index.js
@@ -1,9 +1,39 @@
 import {IHttpConnection} from 'sapps.core.api';
 
+
+function checkStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response
+  } else {
+    var error = new Error(response.statusText)
+    error.response = response
+    throw error
+  }
+}
+
+class Connection{
+    get(url, params) {
+        url = 'api/v1'+url+'.json'; 
+        return fetch(url).then(checkStatus).then(function(response) {
+            return response.json()
+        });
+    }
+    post(url, params){
+
+    }
+    head(url, params){
+
+    }
+    delete(url, params){
+
+    }
+    put(url, params){
+
+    }
+}
+
 export function start(ctx){
-    ctx.services.register(IHttpConnection, {
-        get:fetch
-    });
+    ctx.services.register(IHttpConnection, new Connection());
 }
 
 export function stop(ctx){
